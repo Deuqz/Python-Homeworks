b6eaa2b730ad233c63a720d14ac2a94663cdddce
Deuqz <ddemon2002@mail.ru>
2022-03-09 20:41:41 +0300

    Modify medium

--- hw4/Medium.py
+++ hw4/Medium.py
@@ -10,51 +10,61 @@ def integrate_part(f, a, b, n_iter):
     step = (b - a) / n_iter
     for i in range(n_iter):
         acc += f(a + i * step) * step
-    return acc, f"  integrate [{a}, {b}] starts in {start}\n"
+    return acc, (start, a, b)
 
 
 def integrate_processes(f, a, b, *, n_jobs=1, n_iter=1000):
+    start = time.time()
     n_iter = int(n_iter / n_jobs)
     step = (b - a) / n_jobs
     acc = 0
-    log = f"start work: n_jobs={n_jobs}, start={time.time()}\n"
+    log = []
     with pool.ProcessPoolExecutor(max_workers=n_jobs) as executor:
         futures = [executor.submit(integrate_part, f, a + i * step, a + (i + 1) * step, n_iter)
                    for i in range(n_jobs)]
         for future in pool.as_completed(futures):
             result = future.result()
             acc += result[0]
-            log += result[1]
+            log.append(result[1])
+    time_prog = time.time() - start
     with open("artifacts/MediumLogsProcesses.txt", 'a') as f:
-        f.write(f"{log}\n")
-    return acc
+        f.write(f"start work: n_jobs={n_jobs}, start={start}\n")
+        for elem in log:
+            f.write(f"  integration [{elem[1]}, {elem[2]}] starts in {elem[0]}\n")
+    return acc, time_prog
 
 
 def integrate_threads(f, a, b, *, n_jobs=1, n_iter=1000):
+    start = time.time()
     n_iter = int(n_iter / n_jobs)
     step = (b - a) / n_jobs
     acc = 0
-    log = f"start work: n_jobs={n_jobs}, start={time.time()}\n"
+    log = []
     with pool.ThreadPoolExecutor(max_workers=n_jobs) as executor:
         futures = [executor.submit(integrate_part, f, a + i * step, a + (i + 1) * step, n_iter)
                    for i in range(n_jobs)]
         for future in pool.as_completed(futures):
             result = future.result()
             acc += result[0]
-            log += result[1]
+            log.append(result[1])
+    time_prog = time.time() - start
     with open("artifacts/MediumLogsThreads.txt", 'a') as f:
-        f.write(f"{log}\n")
-    return acc
+        f.write(f"start work: n_jobs={n_jobs}, start={start}\n")
+        for elem in log:
+            f.write(f"  integration [{elem[1]}, {elem[2]}] starts in {elem[0]}\n")
+    return acc, time_prog
 
 
 def run():
+    open("artifacts/MediumComparisonProcesses.txt", 'w').close()
+    open("artifacts/MediumComparisonThreads.txt", 'w').close()
+    open("artifacts/MediumLogsThreads.txt", 'w').close()
+    open("artifacts/MediumLogsProcesses.txt", 'w').close()
     for n_jobs in range(1, 2 * multiprocessing.cpu_count()):
-        start = time.time()
-        integrate_threads(math.cos, 0, math.pi / 2, n_jobs=n_jobs)
+        res = integrate_threads(math.cos, 0, math.pi / 2, n_jobs=n_jobs, n_iter=1000000)
         with open("artifacts/MediumComparisonThreads.txt", 'a') as f:
-            f.write(f"n_jobs={n_jobs} works {time.time() - start}\n")
+            f.write(f"n_jobs={n_jobs} works {res[1]}\n")
     for n_jobs in range(1, 2 * multiprocessing.cpu_count()):
-        start = time.time()
-        integrate_processes(math.cos, 0, math.pi / 2, n_jobs=n_jobs)
+        res = integrate_processes(math.cos, 0, math.pi / 2, n_jobs=n_jobs, n_iter=1000000)
         with open("artifacts/MediumComparisonProcesses.txt", 'a') as f:
-            f.write(f"n_jobs={n_jobs} works {time.time() - start}\n")
+            f.write(f"n_jobs={n_jobs} works {res[1]}\n")
